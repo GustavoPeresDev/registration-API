a09ffcefef1b933213e5aa63eb0e258f97cc6f41
GustavoPeresDev <gustavoperes2004@gmail.com>
2025-10-07 11:24:05 -0300

    primeiro commit

---
+++ src/test/java/http/empresa/cadastro_api/CadastroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package http.empresa.cadastro_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
